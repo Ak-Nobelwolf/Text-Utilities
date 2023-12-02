a7d5d9694cd32006e12c5bf31ff2eaf86f7c98f2
Ak-Nobelwolf <akshaytadakod@gmail.com>
2023-12-03 00:25:50 +0530

    Added few more functionalities

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,14 +3,50 @@ import React, { useState } from "react";
 export default function TextForm(props) {
   const [text, setText] = useState("");
 
+  //Function to Convert to Upper Case
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
   };
 
+  //Function to Convert to Lower Case
   const handleLoClick = () => {
     let newText = text.toLowerCase();
     setText(newText);
+  };
+
+  //Function to Capitalize First Letter of each Sentence
+  const capitalizeFirstLetterOfSentences = () => {
+    let newText = text.replace(/\. *([a-z])/g, (match) => match.toUpperCase());
+    setText(newText);
+  };
+
+  //Function to Capitalize First Letter of each Word
+  const capitalizeFirstLetterOfWords = () => {
+    let newText = text.replace(/\b\w/g, (match) => match.toUpperCase());
+    setText(newText);
+  };
+
+  //Function to Remove Extra Spaces
+  const handleRemoveSpacesClick = () => {
+    let newText = text.replace(/\s+/g, " ");
+    setText(newText);
+  };
+
+  //Function to Reverse the text
+  const handleReverseClick = () => {
+    let newText = text.split("").reverse().join("");
+    setText(newText);
+  };
+
+  //Function to Copy text to Clipboard
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(text);
+  };
+
+  //Function to Convert to Clear the Text Area
+  const handleClearClick = () => {
+    setText("");
   };
 
   const handleOnChange = (event) => {
@@ -36,17 +72,65 @@ export default function TextForm(props) {
           ></textarea>
         </div>
       </div>
-      <button className="btn btn-primary mx-3" onClick={handleUpClick}>
-        Convert to UpperCase
-      </button>
-      <button className="btn btn-primary mx-2" onClick={handleLoClick}>
-        Convert to UpperCase
-      </button>
+
+      <div className="container my-3">
+        <div className="row">
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={handleUpClick}
+          >
+            Convert to UpperCase
+          </button>
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={handleLoClick}
+          >
+            Convert to LowerCase
+          </button>
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={capitalizeFirstLetterOfSentences}
+          >
+            Capitalize Sentences
+          </button>
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={capitalizeFirstLetterOfWords}
+          >
+            Capitalize Words
+          </button>
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={handleRemoveSpacesClick}
+          >
+            Remove Extra Spaces
+          </button>
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={handleReverseClick}
+          >
+            Reverse Text
+          </button>
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={handleCopyClick}
+          >
+            Copy to Clipboard
+          </button>
+          <button
+            className="btn btn-primary col-md-2 mx-1 my-1"
+            onClick={handleClearClick}
+          >
+            Clear Text
+          </button>
+        </div>
+      </div>
+
       <div className="container my-3">
         <h2>Your Text Summary</h2>
         <p>Word Count = {text ? wordCount : 0} </p>
         <p>Character Count = {text ? charCount : 0}</p>
-        <p> {timeToRead} minutes to read</p>
+        <p> Time taken to read = {timeToRead} minutes </p>
         <h2>Preview</h2>
         <p>{text}</p>
       </div>
